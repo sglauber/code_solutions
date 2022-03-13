e0a5021a0c4a52b97f1e1577149d3194d25bbbc3
Glauber S Santana <glauber.silva14@gmail.com>
2022-03-13 15:10:52 -0300

    add: firstUniqueChar description

--- firstUniqueCharacter.js
+++ firstUniqueCharacter.js
@@ -2,6 +2,8 @@
  * @param {string} s
  * @return {number}
  */
+
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 var firstUniqChar = function (s) {
   const obj = {};
   [...s].forEach((el) => (obj[el] ? obj[el]++ : (obj[el] = 1)));
@@ -13,5 +15,6 @@ var firstUniqChar = function (s) {
   return -1;
 };
 
+// expect lleetccoodde to return index 4;
 const s = "lleetccoodde";
 console.log("Char pos:", firstUniqChar(s));
